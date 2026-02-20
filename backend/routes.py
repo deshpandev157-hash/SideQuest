0f2196eb736293feabaaf8d0c265084c0f955998
Vivek <YOUR_GMAIL@gmail.com>
2026-02-20 21:40:41 +0530

    final render import fix

--- backend/routes.py
+++ backend/routes.py
@@ -1,7 +1,8 @@
 from .models import EpisodeRating
 from external_api import search_tvmaze, search_anime
 from flask import Blueprint, request, jsonify
-from .models import User, Content, Review, EpisodeRating
+from .models import User, Content, Review
+from .external_api import search_tvmaze, search_anime
 from .database import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
